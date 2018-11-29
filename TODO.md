2525d691caba6703ad6194dd10a608c205998107
Timothy D Witham <twitham@sbcglobal.net>
2018-11-28 23:37:26 -0600

    Fixed #5.  Config file can now change default units to display.

--- TODO.md
+++ TODO.md
@@ -1,6 +1,6 @@
 # Things TODO in wview-rrd
 
-* add english/metric units option to interface
+* ship /jscalendar-1.0/ or upgrade to JSCal2 or something else.
 
 * re-write rrdgraphcgi.pl as an actual object module, suitable for
   CPAN, unless this already exits.  Something like CGI::RRDs?
--- cgi-bin/weather.pl
+++ cgi-bin/weather.pl
@@ -1,7 +1,7 @@
 # This config file must be valid perl and end with 1.  Keep it secure.
 
 # Anything in the top of cgi-bin/weather can be changed.  Here is what
-# my station uses for example. -twitham
+# my station uses for example. -twitham@sbcglobal.net
 
 # our $where = '/var/lib/weewx/archive'; # or wherever it is
 # our $primary = 'FF00FF';
@@ -11,5 +11,10 @@ our $name1 = 'Garage';		# extra sensor 1
 our $name2 = 'Attic';		# extra sensor 2
 
 our $link = ' See also: '. a({-href => '/weather/'}, 'wview page');
+
+# # to default all units to metric (see %math in ./weather):
+# %def = qw(temp C speed kph press mbar depth mm);
+# # or to override only one at a time:
+# $def{press} = 'kpa';
 
 1;
